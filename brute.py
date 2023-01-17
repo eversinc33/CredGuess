a37c75235189fa877e155e053b9248ef7a45960d
eversinc33 <51821028+eversinc33@users.noreply.github.com>
2023-01-17 09:37:08 +0100

    Update brute.py

--- brute.py
+++ brute.py
@@ -13,7 +13,7 @@ def ad_timestamp_to_unix(timestamp):
         return datetime.datetime(1601, 1, 1) + datetime.timedelta(seconds=timestamp/10000000)
     return np.nan
 
-def get_users(con, user_dn, password, domain) -> List[LdapUser]:
+def get_users(con, user_dn, password, domain, ou) -> List[LdapUser]:
     results = []
 
     attributes = ['samAccountName', 'pwdLastSet']
@@ -21,7 +21,7 @@ def get_users(con, user_dn, password, domain) -> List[LdapUser]:
 
     try:
         con.simple_bind_s(user_dn, password)
-        res = con.search_s(f"{domain_cn}", ldap.SCOPE_SUBTREE, '(&(objectClass=User)(objectCategory=Person))', attributes)
+        res = con.search_s(f"{ou}{domain_cn}", ldap.SCOPE_SUBTREE, '(&(objectClass=User)(objectCategory=Person))', attributes)
         for dn, entry in res:
             results.append(Ldapuser(dn, entry.get("samAccountName"), entry.get("pwdLastSet")))
     except Exception as error:
@@ -40,16 +40,21 @@ def main():
     parser.add_argument('-p', '--password', metavar="<Password>", type=str, required=True)
     parser.add_argument('--ssl', action="store_true", default=False)
     parser.add_argument('--dc-ip', metavar="<DC IP or FQDN>", type=str, required=True)
+    parser.add_argument('--ou', metavar="OU=Users,OU=Berlin", default="" type=str, required=True)
 
     args = parser.parse_args()
 
     user_dn = args.username
     password = args.password
     domain = user_dn.split('@')[1:]
+    if args.ou != "":
+        ou = args.ou if ou.endwith(',') else f"{args.ou},"
+    else:
+        ou = ""
     protocol = "ldaps" if args.ssl else "ldap"
     con = ldap.initialize(f'{protocol}://{args.dc_ip}')
 
-    users = get_users(con, user_dn, password, domain)
+    users = get_users(con, user_dn, password, domain, ou)
 
     # TODO: get pw according to pw last set
     
